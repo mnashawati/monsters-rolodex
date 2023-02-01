5ea95c910231a4b842b5b04cd2805b026a32a994
Mo Nashawati <mo.nashawati@capgemini.com>
2023-02-01 22:49:15 +0000

    add title state to check flash painting/ rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [title, setTitle] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -26,13 +27,26 @@ const App = () => {
     setSearchField(searchFieldString);
   };
 
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
+
+  console.log("rendered");
+
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">{title}</h1>
       <SearchBox
         className="monsters-search-box"
         placeHolder="search monsters"
         onChangeHandler={onSearchChange}
+      />
+      <br />
+      <SearchBox
+        className="title-search-box"
+        placeHolder="set title"
+        onChangeHandler={onTitleChange}
       />
       <CardList monsters={filteredMonsters} />
     </div>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,14 +1,12 @@
 import "./search-box.styles.css";
 
-const SearchBox = ({ className, placeHolder, onChangeHandler }) => {
-  return (
-    <input
-      className={`search-box ${className}`}
-      type="search"
-      placeholder={placeHolder}
-      onChange={onChangeHandler}
-    />
-  );
-};
+const SearchBox = ({ className, placeHolder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeHolder}
+    onChange={onChangeHandler}
+  />
+);
 
 export default SearchBox;
